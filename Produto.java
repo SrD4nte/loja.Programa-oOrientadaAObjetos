e4f41560f3acecb655d6161c9b7d5659b1e3e5dc
Arthur Ortiz da Silva Bach <126367006+Art-26@users.noreply.github.com>
2025-08-06 16:46:18 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,48 @@
+package Projeto1;
+
+public class Produto {
+	private boolean ativo;
+	private int codigo;
+	private double preco;
+	private int quantidade;
+	
+	public Produto(boolean ativo, int codigo, double preco, int quantidade) {
+		this.ativo = ativo;
+		this.codigo = codigo;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+}
